61d93e317cbc615c008083a599f73062940f85f9
Edword-Sun <2087682983@qq.com>
2025-10-18 14:18:01 +0800

    comment: 评论功能

---
+++ tool/filter/comment.go
@@ -0,0 +1,18 @@
+package filter
+
+type FindComment struct {
+	ID      string
+	MusicID string
+	UserID  string
+
+	StartTime int64
+	EndTime   int64
+}
+
+type UpdateComment struct {
+	ID string
+}
+
+type DeleteComment struct {
+	ID string
+}
