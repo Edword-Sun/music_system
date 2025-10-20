2a5547f8966f98fc825611f77f2ec72173114b55
Edword-Sun <2087682983@qq.com>
2025-10-21 00:06:35 +0800

    user_action_properties: 用户操作功能

--- tool/filter/user_action_properties.go
+++ tool/filter/user_action_properties.go
@@ -1,1 +1,26 @@
 package filter
+
+type FindUserActionProperties struct {
+	ID      string
+	UserID  string
+	MusicID string
+
+	IsActiveThumb bool
+	Thumb         bool
+
+	IsActiveUnThumb bool
+	UnThumb         bool
+
+	IsActiveCollected bool
+	Collected         bool
+
+	IsActiveShare bool
+	Share         bool
+
+	StartTime int64
+	EndTime   int64
+}
+
+type DeleteUserActionProperties struct {
+	ID string
+}
